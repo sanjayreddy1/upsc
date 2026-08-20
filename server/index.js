4e64917a5aec9469822401f239ea5bf9c5f7a5e2
Sanjay Kumar <sanjayjacktomy@gmail.com>
2026-08-20 12:34:12 +0530

    fix: use Express 5 wildcard syntax for catch-all route

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === 'production' || process.env.SERVE_STATIC) {
   const distPath = path.join(__dirname, '../dist');
   app.use(express.static(distPath));
 
-  app.get('*', (req, res) => {
+  app.get('/{*path}', (req, res) => {
     res.sendFile(path.join(distPath, 'index.html'));
   });
 } else {
